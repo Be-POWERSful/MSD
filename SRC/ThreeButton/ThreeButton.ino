64a0c5f89c2d2f9250f881f248ecca6699545e6d
Be-POWERSful <47862735+Be-POWERSful@users.noreply.github.com>
2020-02-11 11:27:56 -0500

    Adding three button and fixing automatic mode

--- SRC/ThreeButton/ThreeButton.ino
+++ SRC/ThreeButton/ThreeButton.ino
@@ -8,15 +8,15 @@ const int Button1 = 10;
 const int Button2 = 2;
 const int LED1 = 13;
 const int LED2 = 9;
-
-// Ground and Power pins - AC pins
-
+const int Button3 = A4;
+const int LED3 = A5;
 
 // Global variables
 const int motorSpeed = 1000;
-const int numSteps = 200;
 const int spareTime = 10;
 const int holdTime = 1000;
+const int fullSteps = 200;
+const int miniSteps = 2;
 volatile byte state = LOW;
 
 // used to test the modes
@@ -36,6 +36,8 @@ void setup() {
   pinMode(LED1, OUTPUT);
   pinMode(Button2, INPUT);
   pinMode(LED2, OUTPUT);
+  pinMode(Button3, INPUT);
+  pinMode(LED3, OUTPUT);
 
   // Setup Ground and Power
   pinMode(A0, OUTPUT);
@@ -56,6 +58,7 @@ void setup() {
 
 int buttonState1 = digitalRead(Button1);
 int buttonState2 = digitalRead(Button2);
+int buttonState3 = digitalRead(Button3);
 
 void loop() {
   // read the input pin:
@@ -67,6 +70,7 @@ void loop() {
   }
   else {
     digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
     automatic();
   }
   delay(spareTime);
@@ -74,25 +78,33 @@ void loop() {
 
 void automatic(){
   // Small steps of alternating motors for appearance of same time
-  for (int i = 0; i < numSteps/2; i++) {
-    Motor1->step(2, FORWARD, DOUBLE);
-    Motor2->step(2, BACKWARD, DOUBLE);
+  for (int i = 0; i < fullSteps/miniSteps; i++) {
+    Motor1->step(miniSteps, FORWARD, DOUBLE);
+    Motor2->step(miniSteps, BACKWARD, DOUBLE);
   } 
   delay(holdTime);
 }
 
 void manual(){
   buttonState2 = digitalRead(Button2);
+  buttonState3 = digitalRead(Button3);
+  
   // Turn on LED to signal user mode
   digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
   if (buttonState2 == HIGH){
     // Turn on second LED to illustrate currently turning
     digitalWrite(LED2, LOW);
-    // Small steps of alternating motors for appearance of same time
-    for (int i = 0; i < numSteps/2; i++) {
-      Motor1->step(2, FORWARD, DOUBLE);
-      Motor2->step(2, BACKWARD, DOUBLE);
-    }
+    // Full quarter rotation of motor one for seperate wheel use
+    Motor1->step(fullSteps, FORWARD, DOUBLE);
+    delay(spareTime);
+    // Turn off second LED as it can be rotated again
+  }
+  else if (buttonState3 == HIGH){
+    // Turn on second LED to illustrate currently turning
+    digitalWrite(LED3, LOW);
+    // Full quarter rotation of motor two for seperate wheel use
+    Motor2->step(fullSteps, BACKWARD, DOUBLE);
     delay(spareTime);
     // Turn off second LED as it can be rotated again
   }
